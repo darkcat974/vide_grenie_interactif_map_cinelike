bb90b013291fb87950312840b245044d73852e70
darkcat974 <91675088+darkcat974@users.noreply.github.com>
2023-07-18 15:31:27 +0200

    adding selectable place and occupied places | next step, placing alll, recup info and send to form

---
+++ multi_page/script.js
@@ -0,0 +1,26 @@
+// get all info from the html actions
+const container = document.querySelector('.container');
+const places = document.querySelectorAll('.row .place:not(.occupied)')
+
+// fixing the price
+let PlacePrice = 10;
+
+function UpdateSelectedCount() {
+    const SelectedPlaces = document.querySelectorAll('.row .place.selected');
+
+    const placeIndex = [...SelectedPlaces].map(function (place){
+        return [...places].indexOf(place);
+    })
+    console.log(placeIndex);
+    localStorage.setItem('SelectedPlaces', JSON.stringify(placeIndex))
+
+    const SelectedCount = SelectedPlaces.length;
+};
+
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('place')
+    && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected')
+    }
+    UpdateSelectedCount();
+});
